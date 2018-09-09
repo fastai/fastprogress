4a12c59aaa339ea11d88a7d17ea50503f7254088
Sylvain Gugger <35901082+sgugger@users.noreply.github.com>
2018-09-09 14:48:53 -0400

    new versino

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ setuptools.setup(
     name="fast_progress",
     version="0.1.0",
     author="Sylvain Gugger",
+	license = "Apache License 2.0",
     description="A nested progress with plotting options for fastai",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fastai/fast_progress",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6"
     ],
 )
