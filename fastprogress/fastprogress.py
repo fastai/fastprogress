0c12fdcbbe0c41c5f7f9a3438bc00cb62ff537d9
zc <zachcaceres@gmail.com>
2018-10-25 12:29:59 -0700

    fix: remove git conflict tags

--- fastprogress/fastprogress.py
+++ fastprogress/fastprogress.py
@@ -109,11 +109,7 @@ class MasterBar():
 def html_progress_bar(value, total, label):
     return f"""
     <div>
-<<<<<<< HEAD
-      <progress value='{value}' max='{total}', style='width:300px; height:20px'></progress>
-=======
       <progress value='{value}' max='{total}', style='width:300px; height:20px; vertical-align: middle;'></progress>
->>>>>>> master
       {label}
     </div>
     """
