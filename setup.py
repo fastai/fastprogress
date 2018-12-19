ee4c2188e4db6f64fa5013958543ad9b1c743851
Stas Bekman <stas@stason.org>
2018-12-18 23:03:21 -0800

    consistent whitespace

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fastprogress",
-    version=__version__,
-    author="Sylvain Gugger",
+    name = "fastprogress",
+    version = __version__,
+    author = "Sylvain Gugger",
     license = "Apache License 2.0",
-    description="A nested progress with plotting options for fastai",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/fastai/fastprogress",
-    packages=setuptools.find_packages(),
-    classifiers=[
+    description = "A nested progress with plotting options for fastai",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/fastai/fastprogress",
+    packages = setuptools.find_packages(),
+    classifiers = [
         "Programming Language :: Python :: 3.6",
         'Programming Language :: Python :: 3.7',
     ],
-    setup_requires   = ['pytest-runner'],
-    tests_require    = ['pytest'],
+    setup_requires = ['pytest-runner'],
+    tests_require = ['pytest'],
     test_suite = 'tests',
 )
