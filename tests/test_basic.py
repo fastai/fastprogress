026f09f89610da3032e041a38015bd9d38addeee
Marcin Kardas <marcin.kardas@gmail.com>
2019-09-23 22:21:05 +0200

    Avoid division by zero in case of empty sequences (#35)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,3 +7,7 @@ import fastprogress
 
 def test_basic():
     assert fastprogress.__version__
+
+def test_empty():
+    with pytest.warns(UserWarning, match='Your generator is empty.'):
+        assert list(fastprogress.progress_bar([])) == []
